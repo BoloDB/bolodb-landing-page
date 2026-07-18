c813f59617b69a2933d1688fa0ec7b4a5ef1da51
Somesh Pawan Kamad <som3shkamad@gmail.com>
2026-07-18 09:31:28 +0530

    fix css temporarily for gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://bolodb.dev',
+  // site: 'https://bolodb.dev',
+  base: '/bolodb-landing-page/',
   integrations: [react(), tailwind(), sitemap()],
   markdown: {
     shikiConfig: {
