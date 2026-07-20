0f610f577eeee792f4167ce11640d69af433b67f
Entropy-rgb <som3shkamad@gmail.com>
2026-07-20 15:06:50 +0530

    move domain and add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://bolodb.pages.dev',
+  site: 'https://bolodb.dev',
   integrations: [react(), tailwind(), sitemap()],
   markdown: {
     shikiConfig: {
